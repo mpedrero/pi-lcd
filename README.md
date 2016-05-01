114dd4952d778750a2b4928a44347244e157b39f
Manuel Pedrero <mpedrero10@gmail.com>
2016-05-01 19:04:41 +0200

    First version
    pi-lcd.cfg can be used to specify refresh intervals, number of pages and
    information displayed in each page.
    Info about hostname, ip, uptime, load and temp is available.
    Update README.md with instructions
    Add example pi-lcd.cfg file

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # pi-lcd
 Program to show misc information with LCD RGB Keypad for RPi
+
+## Usage
+Start the program with `./pi-lcd`
+
+Select button turn the screen on and off, Up/Down buttons change the information pages.
+
+
+## Libraries
+
++ __ConfigParser__ to read the configuration file
++ __re__ to grep information
++ __subprocess__ to execute shell commands
++ __threading__ to handle threads
++ __time__ to handle intervals
++ __traceback__
++ __Adafruit\_CharLCD__ [Available here](https://github.com/adafruit/Adafruit_Python_CharLCD)
+
+## Configuration file
+
+__pi-lcd__ displays misc information in a 16x2 character display. To do this, you can define _pages_, each one with two lines of information. File `pi-lcd.cfg` allows to customize the number of pages and the information showed in each one. To specify the total number of pages, change the parameter `npages` on `settings` group. For each page, a group called `pageX` (being X a number between 0 and npages-1) must be created. Each `pageX` group has exactly two keys:
+
++ __line0__ to specify what it is shown on the upper line of the display.
++ __line1__ to specify what it is shown on the lower line of the display.
+
+In the current version, possible values for `line0` and `line1` are:
+
++ __hostname__: displays Raspberry Pi's hostname
++ __ip__: displays IP (currently on eth0)
++ __load__: load on the last 1m/5m/15m
++ __uptime__: time since Raspberry Pi was powered on
++ __temp__: Temperature in Celsius
+
+This readme will be updated regularly when more values are available.
+
+## Advanced configuration
+
+The update interval of various events can be specified in `pi-lcd.cfg`:
+
++ __model\_interval__: time between info updates.
++ __disp\_interval__: time between display updates.
++ __buttons\_interval__: time between buttons' checks.
+
+All the values are expressed in seconds. Decimal values can be used. Note that lower values increase the CPU requirements for the program.
+
+## Example pi-lcd.cfg file
+
+	[settings]
+	npages           = 4
+	nlines           = 2
+	ncols            = 16
+	model_interval   = 1
+	disp_interval    = 2
+	buttons_interval = 0.02
+	
+	[page0]
+	line0     = hostname
+	line1     = ip
+	
+	[page1]
+	line0     = hostname
+	line1     = load
+	
+	[page2]
+	line0     = hostname
+	line1     = uptime
+	
+	[page3]
+	line0     = hostname
+	line1     = temp
---
+++ pi-lcd.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+import re
+import subprocess
+import threading
+import time
+import traceback
+import Adafruit_CharLCD as LCD
+
+
+# Globals
+npages = 4                          # Number of different info pages
+page = 0                            # Current info page
+enable_display = True                # Display on/off
+button_changed = True               # Has been any button triggered?
+lcd = LCD.Adafruit_CharLCDPlate()   # Object representing LCD Hat
+content = {}
+info = {}
+cfg_parser = ConfigParser.ConfigParser()
+
+
+# Execute a shell command and return the output
+def run_cmd(cmd):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    return output
+
+
+# Get internal IP
+def getIP(device="eth0"):
+    cad_ip = ""
+    cad_ip = run_cmd("ip addr show "+device+" | grep \"inet \" | awk '{print $2}' | cut -d/ -f1")[:-1]
+    return cad_ip
+   
+# Get hostname
+def getHostname():
+    cad_hostname = run_cmd("hostname")
+    return cad_hostname
+    
+# Get uptime
+def getUptime():
+    cad_uptime = run_cmd("awk '{printf(\"%dd %02dh %02dm\",($1/60/60/24),($1/60/60%24),($1/60%60))}' /proc/uptime")
+    return cad_uptime
+    
+# Get temperature
+def getTemp():
+    aux_temp = run_cmd("/opt/vc/bin/vcgencmd measure_temp")
+    cad_temp = re.match(r'temp=(\d+\.\d+)\'C',aux_temp).group(1)
+    return cad_temp
+
+# Get load
+def getLoad():
+    aux_load = run_cmd("uptime")
+    cad_load = ''
+    cad_load += re.match(r'(.+)load average\: (\d+\.\d+), (\d+\.\d+), (\d+\.\d+)',aux_load).group(2) + " "
+    cad_load += re.match(r'(.+)load average\: (\d+\.\d+), (\d+\.\d+), (\d+\.\d+)',aux_load).group(3) + " "
+    cad_load += re.match(r'(.+)load average\: (\d+\.\d+), (\d+\.\d+), (\d+\.\d+)',aux_load).group(4)
+    return cad_load
+    
+
+    
+    
+# Data model worker
+def dataModelWorker(interval=1):
+    while True:
+        info["hostname"] = getHostname()
+        info["ip"]       = getIP()
+        info["load"]     = getLoad()
+        info["uptime"]   = getUptime()
+        info["temp"]     = getTemp()
+        time.sleep(interval)
+        
+    
+# Data display worker
+def dataDisplayWorker(interval=2):
+    global enable_display
+    global button_changed
+    global page
+    global ncols
+    
+    while True:
+        lcd.enable_display(enable_display)
+        if button_changed:
+            button_changed = False
+            lcd.clear()
+            
+        lcd.set_cursor(0,0)
+        lcd.message(info[content['page'+str(page),'line0']])
+        lcd.set_cursor(0,1)
+        lcd.message(info[content['page'+str(page),'line1']])
+        lcd.set_cursor(ncols-1,0)
+        lcd.message(str(page))
+        time.sleep(interval)
+
+        
+# Buttons handler worker        
+def buttonsWorker(interval=0.01):
+    global page
+    global button_changed
+    global enable_display
+    
+    while True:
+        if not button_changed:
+            if lcd.is_pressed(LCD.SELECT):
+                enable_display = not enable_display
+                button_changed = True
+            if lcd.is_pressed(LCD.UP):
+                page = (page - 1)%npages
+                button_changed = True
+            if lcd.is_pressed(LCD.DOWN):
+                page = (page + 1)%npages
+                button_changed = True
+        time.sleep(interval)
+            
+            
+            
+            
+try:
+    cfg_parser.read('pi-lcd.cfg')
+except:
+    print "Can't read pi-lcd.cfg"
+    quit()
+    
+try:
+    npages           = int(cfg_parser.get('settings', 'npages'))
+    nlines           = int(cfg_parser.get('settings', 'nlines'))
+    ncols            = int(cfg_parser.get('settings', 'ncols'))
+    model_interval   = float(cfg_parser.get('settings', 'model_interval'))
+    disp_interval    = float(cfg_parser.get('settings', 'disp_interval'))
+    buttons_interval = float(cfg_parser.get('settings', 'buttons_interval'))
+    
+    for i in range(0,npages):
+        key = 'page'+str(i)
+        print 'Saving:',key
+        content[key,'line0'] = cfg_parser.get(key, 'line0')
+        content[key,'line1'] = cfg_parser.get(key, 'line1')
+       
+except:
+    print "Incorrenct config in pi-lcd.cfg"
+    traceback.print_exc()
+    quit()
+
+
+
+
+# Init and clear display
+lcd.set_color(0.0, 0.0, 0.0)    # Turn off RGB Led
+lcd.clear()                     # Clear display
+
+# Launch workers as daemons
+model     = threading.Thread(target=dataModelWorker, args=(model_interval,), name='Model')
+view      = threading.Thread(target=dataDisplayWorker, args=(disp_interval,), name='View')
+buttons   = threading.Thread(target=buttonsWorker, args=(buttons_interval,), name='Buttons')
+
+model.setDaemon(True)
+view.setDaemon(True)
+buttons.setDaemon(True)
+
+model.start()
+view.start()
+buttons.start()
+
+# Wait loop
+while True:
+    time.sleep(1)
